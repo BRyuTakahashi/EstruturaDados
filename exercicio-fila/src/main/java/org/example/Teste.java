68a45d3f79a2fee55ef1270a485ca54ef6f90027
BRyuTakahashi <bruno.takahashi@sptech.school>
2023-11-08 00:02:55 -0300

    pushing new files

--- exercicio-fila/src/main/java/org/example/Teste.java
+++ exercicio-fila/src/main/java/org/example/Teste.java
@@ -1,24 +1,59 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Teste {
 
-    public Integer separaPositivoNegativoEmOrdem(List<Integer> lista) {
+    public static void main(String[] args) {
+        //2,7,-3,-50,45,-4,30,-21,38
+
+        Integer[] lista = {2,7,-3,-50,45,-4,30,-21,38};
+
+        //2 7 45 30 38 -3 -50 -4 -21
+        separaPositivoNegativoEmOrdem(lista);
+
+        //2 7 45 30 38 -21 -4 -50 -3
+        separaPositivoNegativoOrdemDiferente(lista);
+    }
+    public static void separaPositivoNegativoEmOrdem(Integer[] lista) {
         FilaObj<Integer> fila1 = new FilaObj<>(10);
         FilaObj<Integer> fila2 = new FilaObj<>(10);
+        FilaObj<Integer> filaCompleta = new FilaObj<>(20);
 
-        for(int i = 0; i < lista.size(); i++) {
-            if(lista.get(i) >= 0) {
-                fila1.insert(lista.get(i));
+        for(int i = 0; i < lista.length; i++) {
+            if(lista[i] >= 0) {
+                fila1.insert(lista[i]);
             } else {
-                fila2.insert(lista.get(i));
+                fila2.insert(lista[i]);
             }
         }
-        int ordem = 0;
+
         while(!fila1.isEmpty()) {
-             ordem += fila1.poll();
+            filaCompleta.insert(fila1.poll());
+        }
+        while(!fila2.isEmpty()) {
+            filaCompleta.insert(fila2.poll());
         }
 
+        filaCompleta.exibe();
+    }
+
+    public static void separaPositivoNegativoOrdemDiferente(Integer[] lista) {
+        FilaObj<Integer> fila = new FilaObj<>(20);
+        PilhaObj<Integer> pilha = new PilhaObj<>(10);
+
+        for(int i = 0; i < lista.length; i++) {
+            if(lista[i] >= 0) {
+                fila.insert(lista[i]);
+            } else {
+                pilha.push(lista[i]);
+            }
+        }
+        while(!pilha.isEmpty()) {
+            fila.insert(pilha.pop());
+        }
+
+        fila.exibe();
     }
 }
